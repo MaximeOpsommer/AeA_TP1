f6ce365efa7e6944bc84363e0900750d32b3a6bf
Opsommer Maxime <opsommerm@a13p8.fil.univ-lille1.fr>
2017-01-31 18:19:56 +0100

    ShiftOrResolver fini (première version)

--- src/structure/resolver/ShiftOrResolver.java
+++ src/structure/resolver/ShiftOrResolver.java
@@ -1,5 +1,8 @@
 package structure.resolver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShiftOrResolver {
 	
 	private char[] alphabet;
@@ -28,7 +31,7 @@ public class ShiftOrResolver {
 		return this.matrice;
 	}
 
-	public void buildMatrice(String motif) {
+	private void buildMatrice(String motif) {
 		
 		this.init(motif);
 		
@@ -51,11 +54,11 @@ public class ShiftOrResolver {
 		
 	}
 	
-	public void init(String motif) {
+	private void init(String motif) {
 		this.matrice = new int[this.RNA.length()][motif.length()];
 		this.vecteurs = new int[this.alphabet.length][motif.length()];
 		
-		// mettre les indices des occurences des lettres du mot à 1
+		// mettre les indices des occurences des lettres du mot ï¿½ 1
 		for(int j = 0; j < motif.length(); j++) {
 			for(int i = 0; i < this.alphabet.length; i++) {
 				if(motif.charAt(j) == this.alphabet[i])
@@ -66,7 +69,7 @@ public class ShiftOrResolver {
 		}
 	}
 	
-	public int[] colonneSuivante(int[] colonneActuelle, int index) {
+	private int[] colonneSuivante(int[] colonneActuelle, int index) {
 		int[] col1 = this.shift(colonneActuelle);
 		char c = this.RNA.charAt(index);
 		int i = 0;
@@ -77,7 +80,7 @@ public class ShiftOrResolver {
 		return this.AND(col1, col2);
 	}
 	
-	public int[] shift(int[] colonne) {
+	private int[] shift(int[] colonne) {
 		int[] res = new int[colonne.length];
 		res[0] = 1;
 		for(int i = 1; i < res.length; i++)
@@ -87,7 +90,7 @@ public class ShiftOrResolver {
 	}
 	
 	public int[] AND(int[] col1, int[] col2) {
-		/* On suppose que les 2 colonnes ont la même longueur */
+		/* On suppose que les 2 colonnes ont la mï¿½me longueur */
 		int[] res = new int[col1.length];
 		for(int i = 0; i < res.length; i++) {
 			res[i] = (col1[i] == 1 && col2[i] == 1) ? 1 : 0;
@@ -96,17 +99,34 @@ public class ShiftOrResolver {
 		return res;
 	}
 	
+	public List<Integer> getIndexOccurences(String motif) {
+		List<Integer> res = new ArrayList<Integer>();
+		
+		this.buildMatrice(motif);
+		
+		int tmp = motif.length() - 1;
+		
+		for(int i = tmp; i < this.RNA.length(); i++) {
+			if(this.matrice[i][tmp] == 1)
+				res.add(i);
+		}
+		
+		return res;
+	}
+	
 	public static void main(String[] args) {
 		ShiftOrResolver resolver = new ShiftOrResolver("CUACUAUAUAUC");
-		resolver.buildMatrice("UAUA");
+		List<Integer> list = resolver.getIndexOccurences("UAUA");
 		
-		int[][] matrice = resolver.getMatrice();
+		System.out.println(list);
+		
+		/*int[][] matrice = resolver.getMatrice();
 		for(int i = 0; i < matrice[0].length; i++) {
 			for(int j = 0; j < matrice.length; j++) {
 				System.out.print(matrice[j][i]);
 			}
 			System.out.println();
-		}
+		}*/
 	}
 
 }
